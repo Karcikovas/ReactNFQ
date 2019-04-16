dda57bf0b7e69e0ee36e0a71d0f99cdca2807bf1
Viktoras Karcikovas <45361674+Karcikovas@users.noreply.github.com>
2019-04-16 16:09:18 +0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,23 +3,25 @@ import React from 'react';
 class Card extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       opened: false,
     }
   }
-  
+
   render() {
     const {
-      backgroundImage,
-      title,
-      releaseDate,
-      score,
-      votes,
-      description,
+        setlovMovie,
+        liked,
+        backgroundImage,
+        title,
+        releaseDate,
+        score,
+        votes,
+        description
     } = this.props;
     const { opened } = this.state;
-    
+
     return (
       <div className="card">
         <div
@@ -28,26 +30,26 @@ class Card extends React.Component {
             backgroundImage: `url('${backgroundImage}')`,
           }}
         />
-    
+
         <div className="card__title">
           {title}
         </div>
-    
-        <div className="card__like">
-            <i className="fa fa-heart-o" />
-        </div>
-    
+
+          <div className="card__like" onClick={() => setlovMovie()}>
+              <i className={`fa ${liked ? 'fa-heart' : 'fa-heart-o'}`}/>
+          </div>
+
         <div className="card__subtitle">
             <span>{releaseDate}</span>
             <span>{score} ({votes} votes)</span>
         </div>
-    
+
         <div
           className="card-info__header"
           onClick={() => this.setState({ opened: !opened })}>
           Summary
         </div>
-          
+
           {opened
             ? (
               <div className="card-info__description">
